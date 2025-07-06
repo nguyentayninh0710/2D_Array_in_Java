8d892937aac3317dafa12a1c807727767efafa2f
Huynh Ky Khoi Nguyen <huynhkykhoinguyen0710@gmail.com>
2025-07-06 14:48:16 +0700

    add example 3 and 4

---
+++ Learning_2D_array_demo/src/Demo/example3.java
@@ -0,0 +1,12 @@
+package Demo;
+//Task:
+
+// Write a program to find and print the largest value in a 2D array.
+public class example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Learning_2D_array_demo/src/Demo/example4.java
@@ -0,0 +1,17 @@
+package Demo;
+//Task:
+//
+//Write a program to create and print an identity matrix of size n×n (1s on the main diagonal, 0s elsewhere).
+//Expected output example:
+//	1 0 0
+//	0 1 0
+//	0 0 1
+
+public class example4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
