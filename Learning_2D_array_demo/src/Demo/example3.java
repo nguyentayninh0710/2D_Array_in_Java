aafdae9fb65ecf97621dc8cec342f2fdea5e5c22
phuong3010 <floranguyen@Floras-MacBook-Pro.local>
2025-07-06 15:02:05 +0700

    please fix

--- Learning_2D_array_demo/src/Demo/example3.java
+++ Learning_2D_array_demo/src/Demo/example3.java
@@ -1,4 +1,7 @@
 package Demo;
+
+import java.util.Scanner;
+
 //Task:
 
 // Write a program to find and print the largest value in a 2D array.
@@ -6,6 +9,35 @@ public class example3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of row: ");
+		int row = Integer.parseInt(scanner.nextLine());
+		System.out.println("Enter the number of column: ");
+		int column = Integer.parseInt(scanner.nextLine());
+	
+		
+		int[][] MyArray = new int[row][column];
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				System.out.println("Enter number (row " + i + ", column " + j + "): ");
+				MyArray[i][j] =  Integer.parseInt(scanner.nextLine());
+			}
+		}
+		// check max
+		
+		int max = MyArray[0][0]; 
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				if (MyArray[i][j] > max) {
+					MyArray[i][j] = max;
+				}
+			}
+		}
+		
+		System.out.println("Max: " + max);		
 
 	}
 
