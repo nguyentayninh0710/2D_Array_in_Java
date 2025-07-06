e03caa089430dc462b5b73b2981a679f36ba6a7f
Huynh Ky Khoi Nguyen <huynhkykhoinguyen0710@gmail.com>
2025-07-06 13:35:53 +0700

    add task 1

--- Learning_2D_array_demo/src/Demo/exmaple_1.java
+++ Learning_2D_array_demo/src/Demo/exmaple_1.java
@@ -1,5 +1,12 @@
 package Demo;
-
+//Exercise 1 – Create and Print a Matrix
+//Task:
+//
+//Write a program that declares a 3×3 2D array.
+//
+//Input values from the keyboard.
+//
+//Print the matrix you just entered.
 public class exmaple_1 {
 
 	public static void main(String[] args) {
