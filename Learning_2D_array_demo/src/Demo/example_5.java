139c36df826d52e2c8aa35e77c69e877446f7581
Huynh Ky Khoi Nguyen <huynhkykhoinguyen0710@gmail.com>
2025-07-07 20:54:47 +0700

    ex5

---
+++ Learning_2D_array_demo/src/Demo/example_5.java
@@ -0,0 +1,41 @@
+package Demo;
+//Problem Statement
+//Write a Java program that:
+//
+//Asks the user to input the size of a square matrix n × n (n ≥ 2).
+//
+//Inputs all elements of the matrix from the keyboard.
+//
+//Prints the original matrix.
+//
+//Computes and prints the transpose of the matrix.
+//
+//(Transpose: swap rows with columns.)
+// original: 
+// 4 5 6 7
+// 1 2 0 9
+// 2 3 8 0
+// 9 6 4 3
+
+// Transpose:
+// 4 1 2 9
+// 5 2 3 6
+// 6 0 8 4
+// 7 9 0 3
+//Computes and prints:
+//
+//The sum of each row.
+//
+//The sum of each column.
+//
+//The sum of the main diagonal.
+//
+//The sum of the secondary diagonal (from top-right to bottom-left).
+public class example_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
