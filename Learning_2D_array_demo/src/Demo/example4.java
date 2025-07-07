682e9a4275e27e1d091a19dc671659eb6479e166
phuong3010 <floranguyen@Floras-MacBook-Pro.local>
2025-07-07 20:44:39 +0700

    ex4

--- Learning_2D_array_demo/src/Demo/example4.java
+++ Learning_2D_array_demo/src/Demo/example4.java
@@ -1,4 +1,7 @@
 package Demo;
+
+import java.util.Scanner;
+
 //Task:
 //
 //Write a program to create and print an identity matrix of size n×n (1s on the main diagonal, 0s elsewhere).
@@ -11,6 +14,24 @@ public class example4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of row/column: ");
+		int row = Integer.parseInt(scanner.nextLine());
+		
+		int[][] MyArray = new int[row][row];
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				if(i == j) {
+					System.out.print("1");
+				} else {
+					System.out.print("0");
+				}
+			}
+			System.out.println();
+		}
+		
 
 	}
 
