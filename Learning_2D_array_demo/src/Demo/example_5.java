b1cffd509345a6c03c15c31c09ba2bac8d0b77b0
phuong3010 <floranguyen@Floras-MacBook-Pro.local>
2025-07-08 01:03:25 +0700

    ex5

--- Learning_2D_array_demo/src/Demo/example_5.java
+++ Learning_2D_array_demo/src/Demo/example_5.java
@@ -2,13 +2,13 @@ package Demo;
 //Problem Statement
 //Write a Java program that:
 //
-//Asks the user to input the size of a square matrix n × n (n ≥ 2).
+//Asks the user to input the size of a square matrix n × n (n ≥ 2).  (x)
 //
-//Inputs all elements of the matrix from the keyboard.
+//Inputs all elements of the matrix from the keyboard.   (x)
 //
-//Prints the original matrix.
+//Prints the original matrix.  (x)
 //
-//Computes and prints the transpose of the matrix.
+//Computes and prints the transpose of the matrix.  (x)
 //
 //(Transpose: swap rows with columns.)
 // original: 
@@ -17,6 +17,8 @@ package Demo;
 // 2 3 8 0
 // 9 6 4 3
 
+import java.util.Scanner;
+
 // Transpose:
 // 4 1 2 9
 // 5 2 3 6
@@ -24,17 +26,100 @@ package Demo;
 // 7 9 0 3
 //Computes and prints:
 //
-//The sum of each row.
+//The sum of each row.   (x)
 //
-//The sum of each column.
+//The sum of each column.   (x)
 //
-//The sum of the main diagonal.
+//The sum of the main diagonal.    (x)
 //
 //The sum of the secondary diagonal (from top-right to bottom-left).
 public class example_5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int row;
+		do { 
+			System.out.println("Enter the number of row (larger or equal to 2): ");
+			row = Integer.parseInt(scanner.nextLine());	
+			
+			if(row < 2) {
+				System.out.println("Row needs to be > or = 2!!");
+			}
+			
+		}while(row < 2);	
+
+		int[][] Array = new int[row][row];
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				System.out.print("Enter number at row " + i + ", column " + j + ": ");
+				Array[i][j] =  Integer.parseInt(scanner.nextLine());
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				System.out.print(Array[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		//transposed
+		
+		int[][] ArrayTransposed = new int[row][row];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				ArrayTransposed[i][j] = Array[j][i];
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				System.out.print(ArrayTransposed[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		//sum of each row 
+		
+		for (int i = 0; i < row; i++) {
+			int sumRow = 0;
+			for (int j = 0; j < row; j++) {
+				sumRow += ArrayTransposed[i][j];
+			}
+			System.out.println("Sum of row " + i + ": " + sumRow);
+		}
+		
+		System.out.println();
+		
+		//sum of each column
+		for (int i = 0; i < row; i++) {
+			int sumColumn = 0;
+			for (int j = 0; j < row; j++) {
+				sumColumn += Array[i][j];
+			}
+			System.out.println("Sum of column " + i + ": " + sumColumn);
+		}
+		
+		System.out.println();
+		
+		//sum of diagonal
+		int sumDiagonal = 0;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				if(i == j) {
+					sumDiagonal += ArrayTransposed[i][j];
+				}
+			}
+		}
+		System.out.println("Sum of diagonal: "+ sumDiagonal);
+		//
 
 	}
 
